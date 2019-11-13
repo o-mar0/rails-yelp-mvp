c46267ce26cbc3f45ffab7b9bd4ae378d8defa0a
Omar Kassem <omar.reda.kassem@gmail.com>
2019-11-13 15:04:35 +1100

    adding my wonderful restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# THE ROUTES
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show new create]
+
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'pizza',
+    address:      'Cairo',
+    category:     'italian',
+    phone_number: '0481 345 678'
+  },
+  {
+    name:         'Ribs and Burgers',
+    address:      'London',
+    category:     'french',
+    phone_number: '0481 654 322'
+  },
+  {
+    name:         'sushi',
+    address:      'Melbourn',
+    category:     'japanese',
+    phone_number: '0481 123 321'
+  },
+  {
+    name:         'Belgian cuisine',
+    address:      'Melbourn',
+    category:     'belgian',
+    phone_number: '0481 123 321'
+  },
+  {
+    name:         'Noodles',
+    address:      'Melbourn',
+    category:     'chinese',
+    phone_number: '0481 432 456'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
